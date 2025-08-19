4e260bb7663dbbea1233e49ee73a82d8b7c08a9e
janhavi mane <manejanhavi70@gmail.com>
2025-08-20 00:19:09 +0530

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class LeadersInArray {
+    public static List<Integer> findLeaders(int[] arr) {
+        int n = arr.length;
+        List<Integer> leaders = new ArrayList<>(); 
+        int maxRight = arr[n - 1]; // last element is always a leader
+        leaders.add(maxRight);
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxRight) {
+                leaders.add(arr[i]);
+                maxRight = arr[i];
+            }
+        }
+        Collections.reverse(leaders);
+        return leaders;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {16, 17, 4, 3, 5, 2};
+        System.out.println("Leaders: " + findLeaders(arr1)); 
+
+        int[] arr2 = {1, 2, 3, 4, 0};
+        System.out.println("Leaders: " + findLeaders(arr2)); 
+
+        int[] arr3 = {7, 10, 4, 10, 6, 5, 2};
+        System.out.println("Leaders: " + findLeaders(arr3)); 
+
+        int[] arr4 = {5};
+        System.out.println("Leaders: " + findLeaders(arr4)); 
+
+        int[] arr5 = {100, 50, 20, 10};
+        System.out.println("Leaders: " + findLeaders(arr5));
+    }
+}
